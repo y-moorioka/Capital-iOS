3e9e7cd6e77f17a43a60f6b4db369e6ce75b225d
森岡 勇樹 <y-morioka@tsc-security.co.jp>
2021-07-09 23:34:20 +0900

    fix

--- CommonWallet/Classes/Common/Views/Assets/ActionsCollectionViewCell.swift
+++ CommonWallet/Classes/Common/Views/Assets/ActionsCollectionViewCell.swift
@@ -14,6 +14,7 @@ final class ActionsCollectionViewCell: UICollectionViewCell {
     private(set) var actionsViewModel: ActionsViewModelProtocol?
     
     weak var delegate: AlertPresentable?
+    private var notifyAuth = false
 
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -23,15 +24,20 @@ final class ActionsCollectionViewCell: UICollectionViewCell {
 
     @IBAction private func actionSend() {
         if let actionsViewModel = actionsViewModel {
-            self.delegate?.showAlert(title: L10n.Notify.confirmTitle, message: L10n.Notify.confirmDescription, actions: [(L10n.Notify.confirmNo, .cancel), (L10n.Notify.confirmYes, .default)], completion: { Int in
-                if Int == 0 {
-                    try? actionsViewModel.send.command.execute()
-                } else {
-                    if #available(iOS 10.0, *) {
-                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+            notifyCheck()
+            if !notifyAuth {
+                self.delegate?.showAlert(title: L10n.Notify.confirmTitle, message: L10n.Notify.confirmDescription, actions: [(L10n.Notify.confirmNo, .cancel), (L10n.Notify.confirmYes, .default)], completion: { Int in
+                    if Int == 0 {
+                        try? actionsViewModel.send.command.execute()
+                    } else {
+                        if #available(iOS 10.0, *) {
+                            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+                        }
                     }
-                }
-            })
+                })
+            } else {
+                try? actionsViewModel.send.command.execute()
+            }
         }
     }
 
@@ -60,6 +66,21 @@ extension ActionsCollectionViewCell: WalletViewProtocol {
 
             sendButton.invalidateLayout()
             receiveButton.invalidateLayout()
+        }
+    }
+    
+    func notifyCheck() {
+        if #available(iOS 10.0, *) {
+            let semaphore = DispatchSemaphore(value: 0)
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == .authorized {
+                    self.notifyAuth = true
+                } else {
+                    self.notifyAuth = false
+                }
+                semaphore.signal()
+            }
+            semaphore.wait()
         }
     }
 }
